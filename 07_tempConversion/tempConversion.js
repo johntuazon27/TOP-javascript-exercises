74bee9542087f15c174585c10fb5c46dd2c6c8b4
johntuazon27 <54359149+johntuazon27@users.noreply.github.com>
2024-07-25 20:55:50 +0900

    Solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+//  the toFixed returns a string, so we mush parseFloat it
+const convertToCelsius = function(num) {
+  return parseFloat(((num - 32) * (5/9) ).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  return parseFloat(((num*(9/5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
